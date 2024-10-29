5b9d8ecd3fc9f07d9ee228da47d34016afe0fd3c
d13g0-rar-dev <diego2210063@correo.uis.edu.co>
2024-10-29 16:57:06 -0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,2 +1,15 @@
-const API_URL = 'http://localhost:8000/api/v1';
+const API_URL = 'http://localhost:8080/api';
+
+export const getProducts = async () => {
+    const response = await fetch(`${API_URL}/products/all`);
+    return response.json();
+};
+
+export const getProduct = async (id) => {
+    const response = await fetch(`${API_URL}/products/${id}`);
+    return response.json();
+};
+
+
+
 
